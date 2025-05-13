4588744f6104dfb2058b0345ea803a6639eebd15
Azionne <a.vorrice@gmail.com>
2025-05-13 10:47:32 -0400

    util folder error.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,57 @@
 const User = require("../models/user");
-//GET /users
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  DEFAULT_SERVER_ERROR_MESSAGE,
+  BAD_REQUEST_MESSAGE,
+  NOT_FOUND_MESSAGE,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: DEFAULT_SERVER_ERROR_MESSAGE });
     });
 };
 
-//POST /users
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
+
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((users) => res.status(201).send(users))
     .catch((err) => {
-      console.err(err);
+      console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(BAD_REQUEST).send({ message: BAD_REQUEST_MESSAGE });
       }
       return res
-        .status(500)
-        .send({ message: "An error occured on the server." });
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: DEFAULT_SERVER_ERROR_MESSAGE });
     });
 };
 
 const getUser = (req, res) => {
   const { userId } = req.params;
+
   User.findById(userId)
     .orFail()
-    .then((user) => res.status(201).send(user))
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "User not found" });
-      } else if (err.name === "CastError") {
-        return res.status(400).send({ message: "Invalid user ID format" });
+        return res.status(NOT_FOUND).send({ message: NOT_FOUND_MESSAGE });
       }
-      return res.status(500).send({ message: err.message });
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: BAD_REQUEST_MESSAGE });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: DEFAULT_SERVER_ERROR_MESSAGE });
     });
 };
 
