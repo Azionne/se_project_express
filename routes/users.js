c8047f6252fc8db178da561d50cab91cd92f5419
Azionne <a.vorrice@gmail.com>
2025-05-22 08:38:21 -0400

    routes/users update

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,16 @@ const {
   createUser,
   getUserById,
 } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
+// Signup route (no auth)
+router.post("/", createUser);
+
+// Protected routes
+router.get("/me", auth, getCurrentUser);
+router.patch("/me/update", auth, updateProfile);
+router.get("/:id", auth, getUserById);
+
 module.exports = router;
-router.get("/me", getCurrentUser);
-router.patch("/me/update", updateProfile);
-router.get("/:id", getUserById);
