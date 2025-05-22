d643035380dbea980894a61690283291051e27d3
Azionne <a.vorrice@gmail.com>
2025-05-22 08:04:54 -0400

    index.js router updates

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,19 @@ const {
 const usersRoute = require("./users");
 const clothingItem = require("./clothingItem");
 const auth = require("../middlewares/auth.js");
-//const router = require("./routes/index");
 
-router.use("/items", clothingItem);
-
-// POST /signin and /signup handlers
+// Public routes
 router.post("/signin", login);
 router.post("/signup", createUser);
+router.use("/items", clothingItem);
+
+// Protect all routes below this line
+router.use(auth);
+
 router.use("/users", usersRoute);
 router.get("/me", getCurrentUser);
 router.patch("/me", updateProfile);
-router.use(auth);
+
 router.use((req, res) => {
   res.status(404).send({ message: "Router not found" });
 });
