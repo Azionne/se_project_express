a507252b448d9d64574eb3afedda5a8522e80760
Azionne <a.vorrice@gmail.com>
2025-05-23 11:07:55 -0400

    req.user added

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const mainRouter = require("./routes/index");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-app.use(cors()); // CORS should be before routes
-app.use(express.json()); // Parse JSON before routes
+app.use(cors());
+app.use(express.json());
 
-app.use("/", mainRouter); // Mount main
+// Set req.user for all requests (as required by assignment)
+app.use((req, res, next) => {
+  req.user = { _id: "5d8b8592978f8bd833ca8133" };
+  next();
+});
+
+app.use("/", mainRouter);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
